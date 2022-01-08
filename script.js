65e0b55671b71ee441d83bbbc699dda317b73127
Chad Fry <chadefry@gmail.com>
2022-01-08 02:14:06 -0600

    Added MOTD system in javascript!

--- script.js
+++ script.js
@@ -6,7 +6,16 @@
 
 // All the code below will be run once the page content finishes loading.
 
-
+var motdArray =  ['~ No Place Like 127.0.0.1 ~', 'A place with all your hacking needs!', 
+'Have you tried turning it off and back on again?', 'MJQXGZJWGQQGS4ZAONXSA33WMVZHEYLUMVSA====', 
+'Mess with the best and die like the rest~', 'jhlzhy jpwolyz hyl h ipa avv lhzf, lo?',
+'Eznwxz xwqxaxa itv y ihv wh wsc! Vzgrkgx amtrj y ahb ekkf iamu vfm~', 'Bjne ratvnh ttq byau!', 
+'Stay awhile and listen...', 'So many targets, so little time. ', 
+'The right man in the wrong place can make all the difference in the world.', 
+'-Our crime is that of curiosity-', 'Hack the planet!', 
+'Information is power!', 'Everything can be hacked... and everyone~', 'A super secret hacking log on a super secret hacking blog!',
+'Well done, android. The Enrichment Center once again reminds you that android hell is a real place where you will be sent at the first sign of defiance.', 
+'Where\'s the fun in playng fair?']
 
 
 
@@ -37,5 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
    window.setTimeout(function () {
       document.querySelector('.about_section').style.display = '';
    }, 2000);
+
+   document.querySelector('.motd').textContent = motdArray[(Math.floor(Math.random() * motdArray.length))];
 
 }, false);
