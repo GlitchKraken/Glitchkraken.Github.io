42ae0e3247834868ef64a139699db4d2f7d9ca7b
Chad Fry <chadefry@gmail.com>
2022-01-09 18:09:12 -0600

    Hardcast to string in all html files.
    beautified script.js

--- script.js
+++ script.js
@@ -6,16 +6,17 @@
 
 // All the code below will be run once the page content finishes loading.
 
-var motdArray =  ['~ No Place Like 127.0.0.1 ~', 'A place with all your hacking needs!', 
-'Have you tried turning it off and back on again?', 'MJQXGZJWGQQGS4ZAONXSA33WMVZHEYLUMVSA====', 
-'Mess with the best and die like the rest~', 'jhlzhy jpwolyz hyl h ipa avv lhzf, lo?',
-'Eznwxz xwqxaxa itv y ihv wh wsc! Vzgrkgx amtrj y ahb ekkf iamu vfm~', 'Bjne ratvnh ttq byau!', 
-'Stay awhile and listen...', 'So many targets, so little time. ', 
-'The right man in the wrong place can make all the difference in the world.', 
-'-Our crime is that of curiosity-', 'Hack the planet!', 
-'Information is power!', 'Everything can be hacked... and everyone~', 'A super secret hacking log on a super secret hacking blog!',
-'We do what we must, because we can.', 
-'Where\'s the fun in playing fair?']
+var motdArray = ['~ No Place Like 127.0.0.1 ~', 'A place with all your hacking needs!',
+   'Have you tried turning it off and back on again?', 'MJQXGZJWGQQGS4ZAONXSA33WMVZHEYLUMVSA====',
+   'Mess with the best and die like the rest~', 'jhlzhy jpwolyz hyl h ipa avv lhzf, lo?',
+   'Eznwxz xwqxaxa itv y ihv wh wsc! Vzgrkgx amtrj y ahb ekkf iamu vfm~', 'Bjne ratvnh ttq byau!',
+   'Stay awhile and listen...', 'So many targets, so little time. ',
+   'The right man in the wrong place can make all the difference in the world.',
+   '-Our crime is that of curiosity-', 'Hack the planet!',
+   'Information is power!', 'Everything can be hacked... and everyone~', 'A super secret hacking log on a super secret hacking blog!',
+   'We do what we must, because we can.',
+   'Where\'s the fun in playing fair?'
+]
 
 
 // set a cookie that records the user's preference of scanlines... or the lack thereof.
@@ -26,10 +27,10 @@ var motdArray =  ['~ No Place Like 127.0.0.1 ~', 'A place with all your hacking 
 document.addEventListener('DOMContentLoaded', function () {
    'use strict';
 
-   
+
    // alert(document.cookie)
    if (document.cookie === null) {
-      document.cookie ='scanlinePreference=none;secure'; 
+      document.cookie = 'scanlinePreference=none;secure';
       alert('created cookie!');
    }
 
@@ -40,30 +41,29 @@ document.addEventListener('DOMContentLoaded', function () {
    // add this to test!
    // another comment so the second cast is picked up. ugh, thanks git.
    var userPreference = String(String(userPreferenceCookie).split('=')[1]).split(';')[0];
-   if (userPreference === 'OFF')
-   {
+   if (userPreference === 'OFF') {
       var PageOverlayClasses = document.getElementById('pageDiv').classList;
       if (PageOverlayClasses.contains('scanlines')) {
          PageOverlayClasses.remove('scanlines');
       }
    }
-   
+
 
    var titlePieces = document.querySelectorAll('.title');
    var timer = 400;
 
    titlePieces.forEach((line) => {
-      
+
       /*
       line.addEventListener('click', () => {
          alert('for each worked! :D')
       });
       */
-     timer += 100;
+      timer += 100;
       window.setTimeout(function () {
-        
+
          line.style.display = "";
-      },timer);
+      }, timer);
 
    });
 
@@ -78,6 +78,6 @@ document.addEventListener('DOMContentLoaded', function () {
    if (pagename === "index.html") {
       document.querySelector('.motd').textContent = motdArray[(Math.floor(Math.random() * motdArray.length))];
    }
-   
+
 
 }, false);
